af87c9555dfe56f5382a16d4c3309102ee581b13
kevin-shannon <kevers429@gmail.com>
2023-03-22 06:22:22 -0400

    custom Select component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,10 @@
 import './App.css';
-import Book from './Book.js'
+import React, { useState } from 'react';
+import Select from 'react-select';
+import _ from "lodash";
+import BetsBlock from './BetsBlock';
+import BooksBlock from './BooksBlock';
+import { components } from "react-select";
 
 function App() {
   return <FilterableBetTable bets={BETS} />;
@@ -16,55 +21,6 @@ function BetRow({ bet }) {
       <td><BooksBlock bet={bet} /></td>
     </tr>
   );
-}
-
-function BooksBlock({ bet }) {
-  const books_a = [];
-  const books_b = [];
-  bet.outcomes[0].books.forEach((book) => {
-    books_a.push(
-      <Book
-        book={book}
-        key={book} />
-    )
-  })
-  bet.outcomes[1].books.forEach((book) => {
-    books_b.push(
-      <Book
-        book={book}
-        key={book} />
-    )
-  })
-
-  return (
-    <table>
-      <tbody>
-        <tr>
-          <td>{bet.outcomes[0].odds}</td>
-          <td>{books_a}</td>
-        </tr>
-        <tr>
-          <td>{bet.outcomes[1].odds}</td>
-          <td>{books_b}</td>
-        </tr>
-      </tbody>
-    </table>
-  )
-}
-
-function BetsBlock({ bet }) {
-  return (
-    <table>
-      <tbody>
-        <tr>
-          <td>{bet.outcomes[0].name}</td>
-        </tr>
-        <tr>
-          <td>{bet.outcomes[1].name}</td>
-        </tr>
-      </tbody>
-    </table>
-  )
 }
 
 function BetTable({ bets }) {
@@ -97,10 +53,106 @@ function BetTable({ bets }) {
 function FilterableBetTable({ bets }) {
   return (
     <div>
+      <FilterBar />
       <BetTable bets={bets} />
     </div>
   );
 }
+
+const Option = (props) => {
+  return (
+    <div>
+      <components.Option {...props}>
+        <input
+          type="checkbox"
+          checked={props.isSelected}
+          onChange={() => null}
+        />
+        <label>{props.label}</label>
+      </components.Option>
+    </div>
+  );
+};
+
+const ValueContainer = ({ children, ...props }) => {
+  const { getValue, hasValue } = props;
+  const newChildren = [];
+  const nbValues = getValue().length;
+  if (nbValues === 1) {
+    newChildren.push(children[0][0].key.replace(/-.*/g, ""));
+  } else {
+    newChildren.push(`${nbValues} items selected`);
+  }
+  newChildren.push(children[1]);
+
+  if (!hasValue) {
+    return (
+      <components.ValueContainer {...props}>
+        {children}
+      </components.ValueContainer>
+    );
+  }
+  return (
+    <components.ValueContainer {...props}>
+      {newChildren}
+    </components.ValueContainer>
+  );
+};
+
+function BookSelect() {
+  return (
+    <Select 
+      isMulti={true}
+      components={{
+        MultiValueContainer: () => null,
+        Option,
+        ValueContainer: ValueContainer,
+      }}
+      hideSelectedOptions={false}
+      closeMenuOnSelect={false} 
+      isSearchable={false}
+      options={options} />
+  );
+}
+
+function BookFilters() {
+  return (
+    <div style={{display: 'flex'}}>
+      <BookSelect />
+      
+    </div>
+  );
+}
+
+function FilterBar() {
+  const [selectedOption, setSelectedOption] = useState(bet_type_options[1]);
+  return (
+    <div style={{display: 'flex'}}>
+      <Select
+        defaultValue={selectedOption}
+        options={bet_type_options}
+        isSearchable={false}
+        onChange={setSelectedOption} />
+      <BookFilters />
+    </div>
+  );
+}
+
+const bet_type_options = [
+  { value: 'arbitrage', label: 'Arbitrage' },
+  { value: 'free bet', label: 'Free Bet' },
+  { value: 'risk-free bet', label: 'Risk-Free Bet'}
+];
+
+const options = [
+  { value: 'betmgm', label: 'BetMgm', key: 1 },
+  { value: 'caesars', label: 'Caesars', key: 2 },
+  { value: 'draftkings', label: 'Draftkings', key: 3 },
+  { value: 'fanduel', label: 'Fanduel', key: 4 },
+  { value: 'pointsbet', label: 'Pointsbet', key: 5 },
+  { value: 'superbook', label: 'Superbook', key: 6 },
+  { value: 'unibet', label: 'Unibet', key: 7 },
+];
 
 const BETS = [
   {sport: 'nhl', event: 'ANA Ducks vs VAN Canucks', market: 'Total: 10.0', outcomes: Array(2), ev: 95.36, conversion: 48.10, outcomes: [{name: 'Over', odds: 850, books: ['unibet']}, {name: 'Under', odds: -1667, books: ['unibet']}]},
