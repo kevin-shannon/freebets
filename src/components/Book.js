103fd1b65561e36bb24ff2e305daed68ee4b77ab
kevin-shannon <kevers429@gmail.com>
2023-03-24 08:07:35 -0400

    public url

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,4 +1,4 @@
 export default function Book({ book }) {
-  const src = "books/" + book + ".png";
+  const src = process.env.PUBLIC_URL + "/books/" + book + ".png";
   return <img className="book" src={src} alt={book} />;
 }
