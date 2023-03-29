e15516bd64084715821f0389f8c948463150b761
kevin-shannon <kevers429@gmail.com>
2023-03-29 19:11:37 -0400

    unibet exception handling and event date

--- src/scrape/books/unibet.py
+++ src/scrape/books/unibet.py
@@ -1,5 +1,17 @@
 import requests
-from utils import convert_event_name_nhl, convert_team_name_nhl, construct_total_market_name, construct_spread_market_name, construct_team_total_market_name, convert_spread_nhl
+from datetime import datetime
+
+from utils import construct_spread_market_name
+from utils import construct_team_total_market_name
+from utils import construct_total_market_name
+from utils import convert_event_name_nhl
+from utils import convert_spread_nhl
+from utils import convert_team_name_nhl
+
+
+MONEYLINE = 'Moneyline - Inc. OT and Shootout'
+TOTAL = 'Total Goals - Including Overtime and Penalty Shootout'
+SPREAD = 'Handicap - Including Overtime and Penalty Shootout'
 
 def generate_unibet():
     return {
@@ -9,38 +21,82 @@ def generate_unibet():
 # UNIBET
 # NHL
 def generate_unibet_nhl_formatted_events():
-    url = 'https://eu-offering-api.kambicdn.com/offering/v2018/ubusva/listView/ice_hockey/nhl/all/all/matches.json?lang=en_US&market=US-VA&useCombined=true&useCombinedLive=true'
-    res = requests.get(url).json()
     formatted_events = {}
-    events = res['events']
-    for event in events:
-        event_name = convert_event_name_nhl(event['event']['name'])
-        event_id = event['event']['id']
-        formatted_events[event_name] = {}
-        for offer in event['betOffers']:
-            if offer['criterion']['label'] == 'Moneyline - Inc. OT and Shootout':
-                formatted_events[event_name]['Moneyline'] = [{'name': convert_team_name_nhl(outcome['label']), 'odds': int(outcome['oddsAmerican'])} for outcome in offer['outcomes']]
-        url = f'https://eu-offering-api.kambicdn.com/offering/v2018/ubusva/betoffer/event/{event_id}.json'
+    url = 'https://eu-offering-api.kambicdn.com/offering/v2018/ubusva/listView/ice_hockey/nhl/all/all/matches.json?lang=en_US&market=US-VA&useCombined=true&useCombinedLive=true'
+    try:
         res = requests.get(url).json()
-        for offer in res['betOffers']:
+    except:
+        print('error getting url')
+        return formatted_events
+    try:
+        events = res['events']
+    except:
+        print('error parsing events')
+        return formatted_events
+    for event in events:
+        try:
+            event_name = convert_event_name_nhl(event['event']['name'])
+            event_id = event['event']['id']
+        except:
+            print('error parsing event info')
+            continue
+        formatted_events[event_name] = {'offers': {}}
+        try:
+            formatted_events[event_name]['start'] = datetime.strptime(event['event']['start'], '%Y-%m-%dT%H:%M:%SZ')
+        except ValueError:
+            print('error parsing date time')
+            formatted_events[event_name]['start'] = None
+
+        url = f'https://eu-offering-api.kambicdn.com/offering/v2018/ubusva/betoffer/event/{event_id}.json'
+        try:
+            res = requests.get(url).json()
+        except:
+            print('error getting url')
+            continue
+        try:
+            offers = res['betOffers']
+        except:
+            print('error missing betOffers')
+            continue
+        for offer in offers:
+            try:
+                label = offer['criterion']['label']
+            except:
+                print('missing label')
+                continue
+            # Moneyline
+            if label == MONEYLINE:
+                try:
+                    formatted_events[event_name]['offers']['Moneyline'] = [{'name': convert_team_name_nhl(outcome['label']), 'odds': int(outcome['oddsAmerican'])} for outcome in offer['outcomes']]
+                except:
+                    print('something went wrong adding moneyline market')
             # Totals
-            if offer['criterion']['label'] == 'Total Goals - Including Overtime and Penalty Shootout':
-                market_name = construct_total_market_name(float(offer['outcomes'][0]['line'])/1000)
-                formatted_events[event_name][market_name] = [{'name': outcome['label'], 'odds': int(outcome['oddsAmerican'])} for outcome in offer['outcomes']]
+            if label == TOTAL:
+                try:
+                    market_name = construct_total_market_name(float(offer['outcomes'][0]['line'])/1000)
+                    formatted_events[event_name]['offers'][market_name] = [{'name': outcome['label'], 'odds': int(outcome['oddsAmerican'])} for outcome in offer['outcomes']]
+                except:
+                    print('something went wrong adding total market')
             # Spreads
-            if offer['criterion']['label'] == 'Handicap - Including Overtime and Penalty Shootout':
-                line = float(offer['outcomes'][0]['line'])/1000
-                if line < 0:
-                    team = offer['outcomes'][0]['label']
-                else:
-                    team = offer['outcomes'][1]['label']
-                    line = -line
-                market_name = construct_spread_market_name(team, line)
-                formatted_events[event_name][market_name] = [{'name': convert_spread_nhl(outcome['label'], market_name), 'odds': int(outcome['oddsAmerican'])} for outcome in offer['outcomes']]
+            if label == SPREAD:
+                try:
+                    line = float(offer['outcomes'][0]['line'])/1000
+                    if line < 0:
+                        team = offer['outcomes'][0]['label']
+                    else:
+                        team = offer['outcomes'][1]['label']
+                        line = -line
+                    market_name = construct_spread_market_name(team, line)
+                    formatted_events[event_name][market_name] = [{'name': convert_spread_nhl(outcome['label'], market_name), 'odds': int(outcome['oddsAmerican'])} for outcome in offer['outcomes']]
+                except:
+                    print('something went wrong adding spread market')
             # Team Totals
-            if 'Total Goals by' in offer['criterion']['label'] and 'Including Overtime and Penalty Shootout' in offer['criterion']['label']:
-                team = convert_team_name_nhl(offer['criterion']['label'].replace('Total Goals by', '').replace('- Including Overtime and Penalty Shootout', ''))
-                line = float(offer['outcomes'][0]['line'])/1000
-                market_name = construct_team_total_market_name(team, line)
-                formatted_events[event_name][market_name] = [{'name': outcome['label'], 'odds': int(outcome['oddsAmerican'])} for outcome in offer['outcomes']]
+            if 'Total Goals by' in label and 'Including Overtime and Penalty Shootout' in label:
+                try:
+                    team = convert_team_name_nhl(offer['criterion']['label'].replace('Total Goals by', '').replace('- Including Overtime and Penalty Shootout', ''))
+                    line = float(offer['outcomes'][0]['line'])/1000
+                    market_name = construct_team_total_market_name(team, line)
+                    formatted_events[event_name][market_name] = [{'name': outcome['label'], 'odds': int(outcome['oddsAmerican'])} for outcome in offer['outcomes']]
+                except:
+                    print('something went wrong adding team total market')
     return formatted_events
