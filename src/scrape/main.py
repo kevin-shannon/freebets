5dd9bd808362996e47db2ed7d12fd6d8a3e3c6b0
kevin-shannon <kevers429@gmail.com>
2023-03-30 01:37:16 -0400

    event start in main

--- src/scrape/main.py
+++ src/scrape/main.py
@@ -1,5 +1,6 @@
 import boto3
 import json
+from datetime import datetime
 from books.betmgm import generate_betmgm
 from books.caesars import generate_caesars
 from books.draftkings import generate_draftkings
@@ -24,11 +25,13 @@ def aggregate(data):
     for book in data:
         for sport in data[book]:
             for event in data[book][sport]:
-                for market in data[book][sport][event]:
+                start = data[book][sport][event]['start']
+                start_string = datetime.strftime(start, '%Y-%m-%dT%H:%M:%SZ') if start is not None else ''
+                for market in data[book][sport][event]['offers']:
                     key = frozenset([sport, event, market])
-                    outcomes = data[book][sport][event][market]
+                    outcomes = data[book][sport][event][market]['offers']
                     if key not in bets:
-                        bets[key] = {'sport': sport, 'event': event, 'market': market}
+                        bets[key] = {'sport': sport, 'event': event, 'market': market, 'start': start_string}
                         bets[key]['outcomes'] = [{'name': outcome['name'], 'books': {}} for outcome in outcomes]
                     for i in range(len(outcomes)):
                         for j in range(len(bets[key]['outcomes'])):
