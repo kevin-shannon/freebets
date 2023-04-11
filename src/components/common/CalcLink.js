9df003d1a7a6c1f2fe3253c006a27921d4a61e8e
kevin-shannon <kevers429@gmail.com>
2023-04-10 18:36:17 -0700

    filter calc input

--- src/components/common/CalcLink.js
+++ src/components/common/CalcLink.js
@@ -22,7 +22,7 @@ const style = {
 
 export default function ModalLink({ bet, betType, mode }) {
   const [open, setOpen] = useState(false);
-  const [amount_a, setAmount_a] = useState(0);
+  const [amount_a, setAmount_a] = useState("");
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
   const func = betType.value === BetType.ARBITRAGE ? computeEv : computeConversion;
@@ -33,6 +33,26 @@ export default function ModalLink({ bet, betType, mode }) {
   const amount_b = calcHedge(betType, amount_a, odds_a, odds_b)
   const profit = calcProfitNum(betType, amount_a, amount_b, odds_a, odds_b);
   const perc = calcProfitPerc(betType, amount_a, amount_b, profit);
+
+  function handleChange(event) {
+    const input = event.target.value || '';
+    const trimmedInput = input.replace(/^0+/, '');
+    const filteredInput = trimmedInput.replace(/[^\d.-]|(?<=\..*)\./g, '');
+    const decimalIndex = filteredInput.indexOf('.');
+    
+    if (decimalIndex !== -1 && filteredInput.length - decimalIndex > 3) return;
+    if (/e/i.test(filteredInput) || /^-|\+/.test(filteredInput)) return;
+    if (filteredInput !== '' && !/^(\+|-)?(\.)?(\d+)(\.(\d+))?$/g.test(filteredInput) || parseFloat(filteredInput) >= 100000) return;
+    
+    setAmount_a(filteredInput);
+  }
+
+  function handleKeyDown(event) {
+    if (event.key === '-' || event.key === '+' || event.key ==='e' || event.key === 'E') {
+      event.preventDefault();
+    }
+  }
+
   return (
     <div>
       <button className="foot-link" onClick={handleOpen}>
@@ -45,13 +65,13 @@ export default function ModalLink({ bet, betType, mode }) {
       <Modal open={open} onClose={handleClose} disableAutoFocus={true} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
         <Box sx={style}>
           <label>Free Bet</label>
-          <input className="usr-input-bet-amount" value={amount_a} onChange={(event) => { setAmount_a(Number(event.target.value)) }}></input>
+          <input className="usr-input-bet-amount" value={amount_a} type="number" onChange={handleChange} min="0" onKeyDown={handleKeyDown}></input>
           <label>Odds</label>
-          <input value={bet.outcomes[0].odds}></input>
+          <input value={odds_a} readOnly></input>
           <label>Hedge Bet</label>
-          <input value={amount_b}></input>
+          <input type="number" value={amount_b} readOnly></input>
           <label>Odds</label>
-          <input value={bet.outcomes[1].odds}></input>
+          <input value={odds_b} readOnly></input>
           <span>Profit: ${profit[0]} - ${profit[1]}</span>
           <br />
           <span>% Profit: {perc[0]}% - {perc[1]}%</span>
