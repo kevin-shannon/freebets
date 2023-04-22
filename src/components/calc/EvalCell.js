0caa031d44b1e690d6be14bf8a272f1b4c203b43
kevin-shannon <kevers429@gmail.com>
2023-04-22 02:13:43 -0500

    flexbox evalnode

--- src/components/calc/EvalCell.js
+++ src/components/calc/EvalCell.js
@@ -2,37 +2,33 @@ import "./EvalCell.css";
 
 export default function EvalCell({ won, sunk, net, bonus }) {
   return (
-    <div className="eval-cell-container">
-      <table className="eval-cell-table">
-        <tbody>
-          <tr className="eval-cell-row">
-            <td className="row-label won-label">
-              <span>WON</span>
-            </td>
-            <td>{won}</td>
-          </tr>
-          <tr className="eval-cell-row">
-            <td className="row-label sunk-label">
-              <span>SUNK</span>
-            </td>
-            <td>{sunk}</td>
-          </tr>
-          <tr className="eval-cell-row">
-            <td className="row-label net-label">
-              <span>NET</span>
-            </td>
-            <td>{net}</td>
-          </tr>
-          {bonus ? (
-            <tr className="eval-cell-row">
-              <td className="row-label bonus-label">
-                <span>BONUS</span>
-              </td>
-              <td>{bonus}</td>
-            </tr>
-          ) : null}
-        </tbody>
-      </table>
+    <div class="grid-container">
+      <div class="grid-row">
+        <div class="grid-cell row-label won-label">
+          <span>WON</span>
+        </div>
+        <div class="grid-cell">{won}</div>
+      </div>
+      <div class="grid-row">
+        <div class="grid-cell row-label sunk-label">
+          <span>SUNK</span>
+        </div>
+        <div class="grid-cell">{sunk}</div>
+      </div>
+      <div class="grid-row">
+        <div class="grid-cell row-label net-label">
+          <span>NET</span>
+        </div>
+        <div class="grid-cell">{net}</div>
+      </div>
+      {bonus ? (
+        <div class="grid-row">
+          <div class="grid-cell row-label bonus-label">
+            <span>BONUS</span>
+          </div>
+          <div class="grid-cell">{bonus}</div>
+        </div>
+      ) : null}
     </div>
   );
 }
