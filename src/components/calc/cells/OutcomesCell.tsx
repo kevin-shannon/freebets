3a090070c5667d3bbd0cf571179194f5058004e9
kevin-shannon <kevers429@gmail.com>
2023-04-29 07:19:27 -0500

    format outcomes cell money

--- src/components/calc/cells/OutcomesCell.tsx
+++ src/components/calc/cells/OutcomesCell.tsx
@@ -1,4 +1,5 @@
 import "./OutcomesCell.css";
+import { formatMoneyNumber } from "../../../Utils";
 
 interface OutcomesCellProps {
   bet_a: string;
@@ -15,14 +16,14 @@ export default function OutcomesCell({ bet_a, amount_a, bet_b, amount_b }: Outco
           <span>{bet_a}</span>
         </div>
         <div className="grid-cell amount-cell">
-          <span>{amount_a}</span>
+          <span>{formatMoneyNumber(Number(amount_a), false)}</span>
         </div>
       </div>
       <div className="grid-row">
         <div className="grid-cell outcome-cell">
           <span>{bet_b}</span>
         </div>
-        <div className="grid-cell amount-cell">{amount_b}</div>
+        <div className="grid-cell amount-cell">{formatMoneyNumber(Number(amount_b), false)}</div>
       </div>
     </div>
   );
