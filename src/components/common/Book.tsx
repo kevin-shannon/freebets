14329951ce122569d82db229efa73a05418348d3
kevin-shannon <kevers429@gmail.com>
2023-05-08 06:39:30 -0500

    book name on hover

--- src/components/common/Book.tsx
+++ src/components/common/Book.tsx
@@ -1,7 +1,14 @@
-import React from "react";
 import "./Book.css";
+import { Tooltip } from "react-tooltip";
 
-export default function Book({ book }: {book: string}) {
+export default function Book({ book }: { book: string }) {
   const src = process.env.PUBLIC_URL + "/books/" + book + ".png";
-  return <img className="book" src={src} alt={book} />;
+  return (
+    <div>
+      <span data-tooltip-id="book-tool" style={{ display: "flex" }} data-tooltip-html={book}>
+        <img className="book" src={src} alt={book} />
+      </span>
+      <Tooltip id="book-tool" style={{ backgroundColor: "rgb(65 62 73)", color: "#fff", opacity: 1, borderRadius: "8px" }} place="bottom" />
+    </div>
+  );
 }
