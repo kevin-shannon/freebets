c7089ddc8f49690fd1d752f624fbca8a486466c0
kevin-shannon <kevers429@gmail.com>
2023-07-05 03:41:35 -0500

    fix nextui margins

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/layout/App";
 import "react-tooltip/dist/react-tooltip.css";
-import { NextUIProvider } from "@nextui-org/react";
 
 const rootElement = document.getElementById("root");
 if (rootElement !== null) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <React.StrictMode>
-      <NextUIProvider>
-        <App />
-      </NextUIProvider>
+      <App />
     </React.StrictMode>
   );
 }
