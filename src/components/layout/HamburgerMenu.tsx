a08a6f7111014f8ea5423624e1466000fa157010
kevin-shannon <kevers429@gmail.com>
2023-07-07 03:44:32 -0500

    rid invisible input jank

--- src/components/layout/HamburgerMenu.tsx
+++ src/components/layout/HamburgerMenu.tsx
@@ -1,6 +1,4 @@
 import "./HamburgerMenu.css";
-import { useState } from "react";
-import { ReactComponent as Bars } from "../../icons/bars.svg";
 
 interface HamburgerMenuProps {
   hamburgerActive: boolean;
@@ -8,22 +6,18 @@ interface HamburgerMenuProps {
 }
 
 export default function HamburgerMenu({ hamburgerActive, setHamburgerActive }: HamburgerMenuProps) {
-  const [isSlideVisible, setIsSlideVisible] = useState(false);
-
-  const toggleSlide = () => {
-    setIsSlideVisible(!isSlideVisible);
+  const toggleMenu = () => {
     setHamburgerActive(!hamburgerActive);
   };
 
   return (
     <div className="hamburger-content">
-      <input id="togglenav" className="menu-trigger hidden" type="checkbox" />
-      <label htmlFor="togglenav" onClick={toggleSlide} className="burger-wrapper">
+      <div className={`burger-wrapper ${hamburgerActive ? "active" : ""}`} onClick={toggleMenu}>
         <div className="hamburger"></div>
-      </label>
+      </div>
       <div className="sliding-drawer">
         <div className="sliding-shelf">
-          <div className={`sliding-div ${isSlideVisible ? "visible" : ""}`}></div>
+          <div className={`sliding-div ${hamburgerActive ? "visible" : ""}`}></div>
         </div>
       </div>
     </div>
